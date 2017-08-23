01904a794288274cc3a8ae165aed3c79e2ab0f12
Jonathan Bass <jonathanbass@dimensions.co.uk>
2017-08-23 14:02:30 +0100

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogAsync();
+
+            Console.WriteLine("Enter something:");
+            Console.WriteLine("Enter something else:");
+            Console.WriteLine("Enter again!!!!:");
+            Console.ReadLine();
+        }
+
+        private static void LogAsync()
+        {
+            Task.Run(() => CreateFile());
+        }
+
+        private static void CreateFile()
+        {
+            Thread.Sleep(3000);
+            File.Create(@"C:\development\Learning\pwa\my-new-text.txt");
+        }
+    }
+}
